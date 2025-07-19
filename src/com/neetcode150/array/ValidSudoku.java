e8ed363de3b6bb9aaee3c7da0e6f22764bec5025
Eomaxl <souravsnigdh@gmail.com>
2025-07-19 13:26:20 +0530

    Valid sudoku leetcode 36

---
+++ src/com/neetcode150/array/ValidSudoku.java
@@ -0,0 +1,57 @@
+package com.neetcode150.array;
+
+public class ValidSudoku {
+
+    public boolean isValidSudoku(char[][] board) {
+        int N = 9;
+
+        // Use an array to record the status
+        int[][] rows = new int[N][N];
+        int[][] cols = new int[N][N];
+        int[][] boxes = new int[N][N];
+
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                // Check if the position is filled with number
+                if (board[r][c] == '.') {
+                    continue;
+                }
+                int pos = board[r][c] - '1';
+
+                // Check the row
+                if (rows[r][pos] == 1) {
+                    return false;
+                }
+                rows[r][pos] = 1;
+
+                // Check the column
+                if (cols[c][pos] == 1) {
+                    return false;
+                }
+                cols[c][pos] = 1;
+
+                // Check the box
+                int idx = (r / 3) * 3 + c / 3;
+                if (boxes[idx][pos] == 1) {
+                    return false;
+                }
+                boxes[idx][pos] = 1;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidSudoku sudoku = new ValidSudoku();
+        char[][] board = {{'1','2','.','.','3','.','.','.','.'},
+                {'4','.','.','5','.','.','.','.','.'},
+                {'.','9','8','.','.','.','.','.','3'},
+                {'5','.','.','.','6','.','.','.','4'},
+                {'.','.','.','8','.','3','.','.','5'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','.','.','.','.','.','2','.','.'},
+                {'.','.','.','4','1','9','.','.','8'},
+                {'.','.','.','.','8','.','.','7','9'}};
+        System.out.println(sudoku.isValidSudoku(board));
+        }
+}
