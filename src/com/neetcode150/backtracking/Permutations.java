c70516a57749bf37ce63710c0cccf63e25ce90bc
Eomaxl <souravsnigdh@gmail.com>
2025-07-23 21:35:12 +0530

    Permutations

--- src/com/neetcode150/backtracking/Permutations.java
+++ src/com/neetcode150/backtracking/Permutations.java
@@ -1,2 +1,40 @@
-package com.neetcode150.backtracking;public class Permutations {
+package com.neetcode150.backtracking;
+
+import java.util.*;
+
+public class Permutations {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        Permutations obj = new Permutations();
+        List<List<Integer>> res = obj.permute(nums);
+        for (List<Integer> list : res) {
+            System.out.println(list);
+        }
+    }
+    List<List<Integer>> res;
+    public List<List<Integer>> permute(int[] nums) {
+        res = new ArrayList<>();
+        backtrack(nums, 0);
+        return res;
+    }
+
+    public void backtrack(int[] nums, int idx) {
+        if (idx == nums.length) {
+            List<Integer> perm = new ArrayList<>();
+            for (int num : nums) perm.add(num);
+            res.add(perm);
+            return;
+        }
+        for (int i = idx; i < nums.length; i++) {
+            swap(nums, idx, i);
+            backtrack(nums, idx + 1);
+            swap(nums, idx, i);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
