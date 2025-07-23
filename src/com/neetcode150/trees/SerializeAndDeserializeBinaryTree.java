198855df1b38b6bed7e9ef627b8dac9ab9e02a7e
Eomaxl <souravsnigdh@gmail.com>
2025-07-23 14:02:08 +0530

    Serialize and Deserialize Binary Tree

--- src/com/neetcode150/trees/SerializeAndDeserializeBinaryTree.java
+++ src/com/neetcode150/trees/SerializeAndDeserializeBinaryTree.java
@@ -1,4 +1,85 @@
 package com.neetcode150.trees;
 
+import java.util.*;
+
 public class SerializeAndDeserializeBinaryTree {
+    // DFS & BFS TC : O(n) and SC: O(n)
+    // Encodes a tree to a single string.
+    public String serializeDFS(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        dfsSerialize(root, res);
+        return String.join(",", res);
+    }
+
+    private void dfsSerialize(TreeNode node, List<String> res) {
+        if (node == null) {
+            res.add("N");
+            return;
+        }
+        res.add(String.valueOf(node.val));
+        dfsSerialize(node.left, res);
+        dfsSerialize(node.right, res);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserializeDFS(String data) {
+        String[] vals = data.split(",");
+        int[] i = {0};
+        return dfsDeserialize(vals, i);
+    }
+
+    private TreeNode dfsDeserialize(String[] vals, int[] i) {
+        if (vals[i[0]].equals("N")) {
+            i[0]++;
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.parseInt(vals[i[0]]));
+        i[0]++;
+        node.left = dfsDeserialize(vals, i);
+        node.right = dfsDeserialize(vals, i);
+        return node;
+    }
+
+    public String serializeBFS(TreeNode root) {
+        if (root == null) return "N";
+        StringBuilder res = new StringBuilder();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) {
+                res.append("N,");
+            } else {
+                res.append(node.val).append(",");
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+        }
+        return res.toString();
+    }
+
+    public TreeNode deserializeBFS(String data) {
+        String[] vals = data.split(",");
+        if (vals[0].equals("N")) return null;
+        TreeNode root = new TreeNode(Integer.parseInt(vals[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int index = 1;
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (!vals[index].equals("N")) {
+                node.left = new TreeNode(Integer.parseInt(vals[index]));
+                queue.add(node.left);
+            }
+            index++;
+            if (!vals[index].equals("N")) {
+                node.right = new TreeNode(Integer.parseInt(vals[index]));
+                queue.add(node.right);
+            }
+            index++;
+        }
+        return root;
+    }
 }
