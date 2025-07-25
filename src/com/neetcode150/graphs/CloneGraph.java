e26d8adb66031c5989f94d6c8d4941511c334ff8
Eomaxl <souravsnigdh@gmail.com>
2025-07-25 15:50:42 +0530

    Clone Graph

--- src/com/neetcode150/graphs/CloneGraph.java
+++ src/com/neetcode150/graphs/CloneGraph.java
@@ -1,2 +1,45 @@
-package com.neetcode150.graphs;public class CloneGraph {
+package com.neetcode150.graphs;
+
+import java.util.*;
+
+public class CloneGraph {
+
+    public Node cloneGraph(Node node) {
+        Map<Node, Node> oldToNew = new HashMap<>();
+        Queue<Node> queue = new LinkedList<>();
+        oldToNew.put(node, new Node(node.val));
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            Node curr = queue.poll();
+            for(Node neighbor : curr.neighbors) {
+                if(!oldToNew.containsKey(neighbor)) {
+                    oldToNew.put(neighbor, new Node(neighbor.val));
+                    queue.add(neighbor);
+                }
+                oldToNew.get(curr).neighbors.add(oldToNew.get(neighbor));
+            }
+        }
+        return oldToNew.get(node);
+    }
+}
+
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node(){
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val){
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val, ArrayList<Node> _neighbors){
+        val = _val;
+        neighbors = _neighbors;
+    }
 }
