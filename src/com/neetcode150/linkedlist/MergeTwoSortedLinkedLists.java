633fd29f22f5832b42fb6f450078ea4388415b36
Eomaxl <souravsnigdh@gmail.com>
2025-08-04 20:20:00 +0530

    Merge two lists

---
+++ src/com/neetcode150/linkedlist/MergeTwoSortedLinkedLists.java
@@ -0,0 +1,34 @@
+package com.neetcode150.linkedlist;
+
+/**
+ *
+ *   Input: list1 = [1,2,4], list2 = [1,3,5] Output: [1,1,2,3,4,5]
+ *   Input: list1 = [], list2 = [] Output: []
+ *   Input: list1 = [], list2 = [1,2] Output: [1,2]
+ *
+ * */
+
+public class MergeTwoSortedLinkedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (list1 != null && list2 != null) {
+            if(list1.val < list2.val){
+                curr.next = list1;
+                list1 = list1.next;
+            } else if (list1.val > list2.val){
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        if (list1 != null) {
+            curr.next = list1;
+        } else {
+            curr.next = list2;
+        }
+        return dummy.next;
+    }
+}
