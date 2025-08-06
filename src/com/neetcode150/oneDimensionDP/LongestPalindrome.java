75f843da558587685e57c5a172519e5af73e0086
Eomaxl <souravsnigdh@gmail.com>
2025-08-06 11:44:10 +0530

    Longest palindrome

---
+++ src/com/neetcode150/oneDimensionDP/LongestPalindrome.java
@@ -0,0 +1,40 @@
+package com.neetcode150.oneDimensionDP;
+
+public class LongestPalindrome {
+    public String longestPalindrome(String s) {
+        return helper(s,new StringBuilder(s).reverse().toString());
+    }
+
+    public String helper(String text1, String text2){
+        int n = text1.length();
+        int m = text2.length();
+
+        int[][] dp = new int[n+1][m+1];
+
+        for(int i = 1; i <= n; i++){
+            for (int j = 1; j <= m; j++){
+                if(text1.charAt(i) == text2.charAt(j)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int i = n, j = m;
+
+        while(i >= 0 && j >= 0){
+            if(text1.charAt(i) == text2.charAt(j)){
+                sb.append(text1.charAt(i));
+                i--;
+                j--;
+            } else if(dp[i-1][j] > dp[i][j-1]){
+                i--;
+            } else {
+                j--;
+            }
+        }
+        return sb.reverse().toString();
+    }
+}
