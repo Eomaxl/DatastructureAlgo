31b590ab5b97625d16fa2d4f9a1ea5b50ed91843
Eomaxl <souravsnigdh@gmail.com>
2025-08-06 17:33:25 +0530

    Car fleet

--- src/com/neetcode150/stack/CarFleet.java
+++ src/com/neetcode150/stack/CarFleet.java
@@ -1,6 +1,9 @@
 package com.neetcode150.stack;
 
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * There are n cars traveling to the same destination on a one-lane highway.
  * You are given two arrays of integers position and speed, both of length n.
@@ -19,4 +22,22 @@ package com.neetcode150.stack;
  * The cars starting at 1 and 0 never catch up to the car ahead of them. Thus, there are 3 car fleets that will arrive at the destination.
  * */
 public class CarFleet {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int[][] pair = new int[position.length][2];
+        for (int i = 0; i < position.length; i++) {
+            pair[i][0] = position[i];
+            pair[i][1] = speed[i];
+        }
+        Arrays.sort(pair, (a, b) -> Integer.compare(b[0], a[0]));
+        Stack<Double> stack = new Stack<>();
+        for (int[] p : pair) {
+            stack.push((double) (target - p[0]) / p[1]);
+            if (stack.size() >= 2 &&
+                    stack.peek() <= stack.get(stack.size() - 2))
+            {
+                stack.pop();
+            }
+        }
+        return stack.size();
+    }
 }
