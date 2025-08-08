e2da8ce4d67851fd34cdf0f4464ff73721ce51f7
Eomaxl <souravsnigdh@gmail.com>
2025-08-08 22:03:06 +0530

    Dinners Philosophy

---
+++ src/com/concurrency/DinnersPhilosophy.java
@@ -0,0 +1,54 @@
+package com.concurrency;
+
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+        public class DinnersPhilosophy {
+
+            private Lock leftForkLock = new ReentrantLock();
+            private Lock rightForkLock = new ReentrantLock();
+
+            public DinnersPhilosophy() {
+
+            }
+
+            // call the run() method of any runnable to execute its code
+            public void wantsToEat(int philosopher,
+                                   Runnable pickLeftFork,
+                                   Runnable pickRightFork,
+                                   Runnable eat,
+                                   Runnable putLeftFork,
+                                   Runnable putRightFork) throws InterruptedException {
+
+
+                while(true) {
+                    if(leftForkLock.tryLock(100, TimeUnit.MILLISECONDS)) {
+                        try {
+                            pickLeftFork.run();
+
+                            if (rightForkLock.tryLock(100, TimeUnit.MILLISECONDS)) {
+                                try {
+                                    pickRightFork.run();
+                                    eat.run();
+                                    putRightFork.run();
+
+                                    /*
+                                     * we have eaten
+                                     */
+                                    return;
+                                } finally {
+                                    rightForkLock.unlock();
+                                }
+                            }
+
+
+                        } finally {
+                            putLeftFork.run();
+                            leftForkLock.unlock();
+                        }
+                    }
+                }
+            }
+        }
