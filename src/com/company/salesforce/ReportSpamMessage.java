160851ca83411ad78336a9f3aedefbbab15436c3
Eomaxl <souravsnigdh@gmail.com>
2025-08-24 21:46:20 +0530

    ReportSpam Message

--- src/com/company/salesforce/ReportSpamMessage.java
+++ src/com/company/salesforce/ReportSpamMessage.java
@@ -1,2 +1,24 @@
-package com.company.salesforce;public class ReportSpamMessage {
+package com.company.salesforce;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ReportSpamMessage {
+    public boolean reportSpam(String[] message, String[] bannedWords) {
+        Set<String> bannedWordSet = new HashSet<String>();
+        for(String str: bannedWords){
+            bannedWordSet.add(str);
+        }
+        int count = 0;
+        for(int i = 0; i<message.length; i++){
+            if (count == 2){
+                return true;
+            }
+            if (bannedWordSet.contains(message[i])){
+                count++;
+            }
+        }
+
+        return count == 2 ? true : false;
+    }
 }
