bbff6cc0a9ef11f623bad43f12674db9ecbd707e
Eomaxl <souravsnigdh@gmail.com>
2025-09-10 15:57:15 +0530

    Leetcode: 744: Find the smallest letter greater than target

---
+++ src/com/ProPeers/BinarySearch/BasicBinarySearch/NextGreatestLetter.java
@@ -0,0 +1,40 @@
+package com.ProPeers.BinarySearch.BasicBinarySearch;
+
+/**
+ * You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+ * Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+ *
+ * Example 1:
+ * Input: letters = ["c","f","j"], target = "a"
+ * Output: "c"
+ * Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+ *
+ * Example 2:
+ * Input: letters = ["c","f","j"], target = "c"
+ * Output: "f"
+ * Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
+ *
+ *
+ * Example 3:
+ * Input: letters = ["x","x","y","y"], target = "z"
+ * Output: "x"
+ * Explanation: There are no characters in letters that is lexicographically greater than 'z' so we return letters[0].
+ * */
+
+public class NextGreatestLetter {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int l = 0, r = letters.length - 1, mid;
+        int pos = -1;
+        while ( l <= r) {
+            mid  = l + (r - l)/ 2;
+            if(letters[mid] > target ){
+                pos = mid;
+                r = mid -1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return pos == -1 ? letters[0] : letters[pos];
+    }
+}
